11c01704086b330acca6506312ad54b7fc831a16
novakvova <novakvova@gmail.com>
2026-06-06 15:51:26 +0300

    Add Register user is good. working LoginScreen

--- fifty-four/app/_layout.tsx
+++ fifty-four/app/_layout.tsx
@@ -3,29 +3,60 @@ import {Stack} from 'expo-router';
 import {StatusBar} from 'expo-status-bar';
 import 'react-native-reanimated';
 import '../global.css';
-
 import {useColorScheme} from '@/hooks/use-color-scheme';
 import {Provider} from "react-redux";
 import {setupStore} from "@/store";
-
-// export const unstable_settings = {
-//     anchor: '(tabs)',
-// };
+import * as SecureStore from 'expo-secure-store';
+import {loginSuccess} from "@/store/reducers/AuthSlice";
+import {useEffect, useState} from "react";
 
 const store = setupStore();
 
 export default function RootLayout() {
+
+    //token
+    //await SecureStore.getItemAsync('accessToken');
+    const [storageReady, setStorageReady] = useState(false);
+
+    useEffect(() => {
+        initStore().then(() => {
+            setStorageReady(true)
+        });
+    }, []);
+
+    async function initStore(): Promise<void> {
+        const accessToken  = await SecureStore.getItemAsync('accessToken');
+        // console.log("User info", accessToken);
+        if (accessToken) {
+            store.dispatch(loginSuccess(accessToken));
+            console.log("User info", accessToken);
+        }
+    }
+
+
     const colorScheme = useColorScheme();
+
+
+
+    if (!storageReady) {
+        return null;
+    }
+
     return (
-        <Provider store={store}>
-            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-                <Stack>
-                    <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-                    <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-                    <Stack.Screen name="modal" options={{presentation: 'modal', title: 'Modal'}}/>
-                </Stack>
-                <StatusBar style="auto"/>
-            </ThemeProvider>
-        </Provider>
+        <>
+            <Provider store={store}>
+                <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                    <Stack>
+                        <Stack.Screen name="(auth)" options={{headerShown: false}}/>
+                        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                        <Stack.Screen name="modal" options={{presentation: 'modal', title: 'Modal'}}/>
+                        <Stack.Screen name="logger" options={{headerShown: false}}/>
+                    </Stack>
+                    <StatusBar style="auto"/>
+                </ThemeProvider>
+            </Provider>
+
+        </>
+
     );
 }
